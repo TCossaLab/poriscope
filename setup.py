fa9cb684b2908023a3742d1c8d944f2307e41ee9
Carogg28 <90726597+Carogg28@users.noreply.github.com>
2025-08-26 13:45:15 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -59,7 +59,7 @@ setup(
         "scikit-learn==1.6.1",
         "hdbscan==0.8.40",
     ],
-    python_requires=">=3.12.9",
+    python_requires=">=3.12.10",
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
