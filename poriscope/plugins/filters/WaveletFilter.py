8b6d34c540268cd38c8c9a9fc9e6cdfb8062107a
Carogg28 <90726597+Carogg28@users.noreply.github.com>
2026-03-05 08:14:52 -0500

    Include compiled wavelet when building PyPi wheel

--- poriscope/plugins/filters/WaveletFilter.py
+++ poriscope/plugins/filters/WaveletFilter.py
@@ -25,6 +25,7 @@
 
 
 import ctypes
+import importlib.resources
 import logging
 import os
 import platform
@@ -153,17 +154,8 @@ class WaveletFilter(MetaFilter):
             ext_map = {"Windows": ".dll", "Linux": ".so", "Darwin": ".dylib"}
             if system not in ext_map:
                 raise RuntimeError(f"Unsupported platform: {system}")
-            dll_path = os.path.abspath(
-                os.path.join(
-                    os.path.dirname(__file__),
-                    "..",
-                    "..",
-                    "cdlls",
-                    "wavelet",
-                    "dist",
-                    f"wavelet{ext_map[system]}",
-                )
-            )
+            ref = importlib.resources.files("poriscope") / "cdlls" / "wavelet" / "dist" / f"wavelet{ext_map[system]}"
+            dll_path = os.path.abspath(str(ref))
 
         if not os.path.isfile(dll_path):
             raise FileNotFoundError(
