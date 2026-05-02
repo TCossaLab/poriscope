fixing tricky numerical bug

poriscope/plugins/eventfitters/CUSUM.py
@@ -312,25 +312,32 @@ class CUSUM(MetaEventFitter):
                     gneg[k - 1] + logn, 0
                 )  # accumulate or reset negative decision function
                 if gpos[k] > threshold or gneg[k] > threshold:
+                    jump_accepted = False
+                    
                     if gpos[k] > threshold:  # significant positive jump detected
-                        jump = (
-                            1 + anchor + np.argmin(cpos[anchor : k + 1])
-                        )  # find the location of the start of the jump
+                        jump = 1 + anchor + np.argmin(cpos[anchor : k + 1])
+                        # Note: C also checks `length - jump > rise_time` here, 
+                        # you may want to add that to match C perfectly!
                         if jump - edges[num_states] > rise_time:
                             edges = np.append(edges, jump)
                             num_states += 1
+                            jump_accepted = True
+                            
                     if gneg[k] > threshold:  # significant negative jump detected
                         jump = 1 + anchor + np.argmin(cneg[anchor : k + 1])
                         if jump - edges[num_states] > rise_time:
                             edges = np.append(edges, jump)
                             num_states += 1
-                    anchor = k
-                    cpos[0 : len(cpos)] = 0  # reset all decision arrays
-                    cneg[0 : len(cneg)] = 0
-                    gpos[0 : len(gpos)] = 0
-                    gneg[0 : len(gneg)] = 0
-                    mean = data[anchor]
-                    varM = data[anchor]
+                            jump_accepted = True
+                            
+                    if jump_accepted:
+                        anchor = k
+                        cpos[0 : len(cpos)] = 0  
+                        cneg[0 : len(cneg)] = 0
+                        gpos[0 : len(gpos)] = 0
+                        gneg[0 : len(gneg)] = 0
+                        mean = data[anchor]
+                        varM = data[anchor]
             varS = 0
             edges = np.append(edges, length)  # mark the end of the event as an edge
             num_states += 1