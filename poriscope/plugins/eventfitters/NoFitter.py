off by one errors and index fixes

poriscope/plugins/eventfitters/NoFitter.py
@@ -457,13 +457,13 @@ class NoFitter(MetaEventFitter):
         )
         event_metadata["max_blockage_duration"] = sublevel_metadata[
             "sublevel_duration"
-        ][np.argmax(sublevel_metadata["sublevel_blockage"][1:-1])]
+        ][1:-1][np.argmax(sublevel_metadata["sublevel_blockage"][1:-1])]
         event_metadata["min_blockage_duration"] = sublevel_metadata[
             "sublevel_duration"
-        ][np.argmin(sublevel_metadata["sublevel_blockage"][1:-1])]
+        ][1:-1][np.argmin(sublevel_metadata["sublevel_blockage"][1:-1])]
         event_metadata["max_deviation_duration"] = sublevel_metadata[
             "sublevel_duration"
-        ][np.argmax(sublevel_metadata["sublevel_max_deviation"][1:-1])]
+        ][1:-1][np.argmax(sublevel_metadata["sublevel_max_deviation"][1:-1])]
         event_metadata["baseline_current"] = (
             sublevel_metadata["sublevel_current"][0]
             * sublevel_metadata["sublevel_duration"][0]

poriscope/plugins/eventfitters/PeakFinder.py
@@ -502,7 +502,7 @@ class PeakFinder(MetaEventFitter):
         # This trims the event to start/end at the longest above-threshold blockage
 
         threshold = 3 * baseline_std
-        event_data = data[padding_before:-padding_after]
+        event_data = data[padding_before : -padding_after if padding_after else None]
         above_threshold = (
             np.abs((np.abs(event_data) - np.sign(baseline_mean) * baseline_mean))
             > threshold