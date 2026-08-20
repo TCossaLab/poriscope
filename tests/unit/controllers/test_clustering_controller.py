80921d2a4d1da5a5cfdbfc2e2fc42e2ad7dee2e1
Carogg28 <90726597+Carogg28@users.noreply.github.com>
2026-08-20 08:00:32 -0400

    test: remove tests for unimplemented plugin-refresh feature

--- tests/unit/controllers/test_clustering_controller.py
+++ tests/unit/controllers/test_clustering_controller.py
@@ -94,22 +94,6 @@ def test_init_creates_view_and_model(mocker: MockerFixture) -> None:
     assert ctrl.model is mock_model_cls.return_value
 
 
-def test_setup_connections_connects_request_plugin_refresh(
-    controller: ClusteringController,
-    mock_view: MagicMock,
-) -> None:
-    """
-    Verify that _setup_connections wires request_plugin_refresh to refresh_plugin_list.
-
-    :param controller: Controller under test.
-    :param mock_view: Mocked clustering view.
-    """
-    controller._setup_connections()
-    mock_view.request_plugin_refresh.connect.assert_called_once_with(
-        controller.refresh_plugin_list
-    )
-
-
 # -------------------- display_write_status ---------------------------
 
 
@@ -374,72 +358,3 @@ def test_update_column_units_skips_view_when_units_empty(
     controller.update_column_units({}, "x")
     mock_view.update_column_units.assert_not_called()
 
-
-# ----------------------- update_plugins ------------------------------
-
-
-def test_update_plugins_emits_update_available_plugins(
-    controller: ClusteringController,
-) -> None:
-    """
-    Emit update_available_plugins with MetaDatabaseLoader and the plugin list.
-
-    :param controller: Controller under test.
-    """
-    controller.update_plugins(["plugin_a", "plugin_b"])
-    controller.update_available_plugins.emit.assert_called_once_with(
-        "MetaDatabaseLoader", ["plugin_a", "plugin_b"]
-    )
-
-
-def test_update_plugins_emits_with_empty_list(
-    controller: ClusteringController,
-) -> None:
-    """
-    Emit update_available_plugins with an empty plugin list.
-
-    :param controller: Controller under test.
-    """
-    controller.update_plugins([])
-    controller.update_available_plugins.emit.assert_called_once_with(
-        "MetaDatabaseLoader", []
-    )
-
-
-# -------------------- refresh_plugin_list ----------------------------
-
-
-def test_refresh_plugin_list_emits_global_signal_when_loader_present(
-    controller: ClusteringController,
-    mock_view: MagicMock,
-) -> None:
-    """
-    Emit a global signal to list plugins when a loader is selected in the view.
-
-    :param controller: Controller under test.
-    :param mock_view: Mocked clustering view.
-    """
-    mock_view.clusteringcontrols.get_current_loader.return_value = "MyLoader"
-
-    controller.refresh_plugin_list()
-
-    controller.global_signal.emit.assert_called_once_with(
-        "MetaDatabaseLoader", "MyLoader", "list_plugins", (), "update_plugins", ()
-    )
-
-
-def test_refresh_plugin_list_does_not_emit_when_loader_absent(
-    controller: ClusteringController,
-    mock_view: MagicMock,
-) -> None:
-    """
-    Do not emit a global signal when no loader is currently selected.
-
-    :param controller: Controller under test.
-    :param mock_view: Mocked clustering view.
-    """
-    mock_view.clusteringcontrols.get_current_loader.return_value = None
-
-    controller.refresh_plugin_list()
-
-    controller.global_signal.emit.assert_not_called()
