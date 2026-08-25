5447d3d8162206775073fcd6ce134786b39a378c
nkerr091 <nkerr091@uottawa.ca>
2026-08-25 15:57:50 -0400

    small current direction dependance fix

--- poriscope/plugins/eventfitters/PeakFinder.py
+++ poriscope/plugins/eventfitters/PeakFinder.py
@@ -950,14 +950,8 @@ class PeakFinder(MetaEventFitter):
                             ]  # turned into absolute blockage instead of current
                         ),
                         "prominence": properties["prominences"][i],
-                        "left_base": np.absolute(
-                            np.sign(baseline_mean) * baseline_mean
-                            + properties["left_bases"][i]
-                        ),
-                        "right_base": np.absolute(
-                            np.sign(baseline_mean) * baseline_mean
-                            + properties["right_bases"][i]
-                        ),
+                        "left_base":  np.sign(baseline_mean) * (baseline_mean - properties["left_bases"][i]),
+                        "right_base": np.sign(baseline_mean) * (baseline_mean - properties["right_bases"][i]),
                         "width": properties["widths"][i],
                         "left_ips": left_ip,
                         "right_ips": right_ip,
